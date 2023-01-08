47b57c3d4b902e505e2633cbe0385ce1337dc1c8
Nicolas Temprano <nicomxd22@gmail.com>
2023-01-08 11:29:32 -0300

    primera entrega

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hola coder! 👋</Text>
+      <Text>Ingresa tu nombre:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Button title='Saludar'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 50,
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 200,
+    height: 50,
+    marginVertical: 50,
   },
 });
